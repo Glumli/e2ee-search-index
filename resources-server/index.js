f0975e498387b9725677c6c50bf3b1a824cce188
Glumli <github@glumli.de>
2021-08-07 12:31:42 +0200

    hopefully fix memory issues

--- resources-server/index.js
+++ resources-server/index.js
@@ -2,18 +2,30 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 const process = require("process");
+const fs = require("fs");
 
 app.use(cors());
 
 const PORT = 8080;
 
+const loadResources = (cohort, user) => {
+  const resources = {};
+  fs.readdirSync(`./resources/${cohort}/${user}`).forEach((file) => {
+    if (file.startsWith("testResources")) return;
+
+    const resource = fs.readFileSync(`./resources/${cohort}/${user}/${file}`);
+    resources[file.split(".")[0]] = JSON.parse(resource);
+  });
+  return resources;
+};
+
 app.get("/resources/:cohort/:user", (req, res) => {
   let { cohort, user } = req.params;
   console.log(cohort, user);
   try {
-    let resources = require(`./resources/${cohort}/${user}/testResources.js`);
+    let resources = loadResources(cohort, user);
     res.status(200).json(JSON.stringify(resources));
-    delete resources;
+    resources = {};
   } catch (e) {
     res.status(200).json(JSON.stringify({}));
   }
